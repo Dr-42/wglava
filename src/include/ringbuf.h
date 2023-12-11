c69efdd6f6574d9eacbdfdf3bf831a2d3e63d4e0
Spandan <royspandan1@gmail.com>
2023-12-11 19:44:43 +0530

    fft with lag

--- src/include/ringbuf.h
+++ src/include/ringbuf.h
@@ -1,17 +1,25 @@
 #pragma once
 
-class RingBuf{
+#include <complex>
+
+class RingBuf {
 public:
-	RingBuf(unsigned int size);
-	~RingBuf();
-	void Push(float val);
-	void Cat(float* vals, int num);
-	float Get(int index);
-	unsigned int GetSize();
-	unsigned int GetIndex();
-	float operator [](int index);
+    RingBuf(unsigned int size);
+    ~RingBuf();
+    void Push(float val);
+    void Cat(float* vals, int num);
+    float Get(int index);
+    unsigned int GetSize();
+    unsigned int GetIndex();
+    float operator[](int index);
+    void fft_analyze(float dt);
+    float get_fft(int index);
+	size_t fft_size;
 private:
-	float* buf;
-	unsigned int size;
-	int index;
+    float* buf;
+    std::complex<float>* fft_buf;
+    unsigned int size;
+    int index;
+    void fft(float in[], size_t stride, std::complex<float> out[], size_t n);
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,12 @@ int main() {
         renderer.Begin();
         uint64_t num_frames = ac.fill_buffer();
         rb.Cat(ac.get_data(), num_frames);
-        for (UINT32 i = 0; i < rb.GetSize(); i++) {
-            float sample = rb[i];
-            float x = ((float)i / (float)num_frames) * 2.0f - 1.0f;
-            float y = -0.0f;
-            float w = 1.0f / (float)num_frames;
+        rb.fft_analyze(1.0f / 44100.0f);
+        for (UINT32 i = 0; i < rb.fft_size; i++) {
+            float sample = rb.get_fft(i);
+            float x = ((float)i / (float)rb.fft_size) * 2.0f - 1.0f;
+            float y = -0.5f;
+            float w = 1.0f / (float)rb.fft_size;
             Rect rect = {x, y, w, sample, 0.0f};
             Color color = {1.0f, 0.0f, 1.0f, 1.0f};
             renderer.DrawRect(rect, color);
